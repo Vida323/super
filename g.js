d2847afacbf36c55558277cabe6e684c0e5e2324
Vida323 <37506386+Vida323@users.noreply.github.com>
2018-07-31 08:04:21 -0400

    Create g.js

---
+++ g.js
@@ -0,0 +1,11 @@
+var g = {
+  id : function(id) {
+    return document.getElementById(id);
+  },
+  cn : function(cn) {
+    return document.getElementsByClassName(cn);
+  },
+  tn : function(tn) {
+    return document.getElementsByClassName(tn);
+  }
+}
